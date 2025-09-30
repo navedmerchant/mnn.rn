3e9ce698d815e17941e850d4ed5bc1af66aeb246
Naved Merchant <naved.merchant@gmail.com>
2025-09-29 23:34:01 -0700

    Fist commit. builds successfully

---
+++ ios/MnnRn.h
@@ -0,0 +1,5 @@
+#import <MnnRnSpec/MnnRnSpec.h>
+
+@interface MnnRn : NSObject <NativeMnnRnSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import MnnRn from './NativeMnnRn';
+
+export function multiply(a: number, b: number): number {
+  return MnnRn.multiply(a, b);
+}
